55ceddf1eb54b4349facb44bbf1dd94b0df4883d
dayanday <sszhangy@163.com>
2014-05-14 10:02:46 +0800

    v1.0 added

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main(
 		System.out.println("version v1.0");
 		System.out.println("author dnd");
 		System.out.println("time 20140513");
+		
+		//branch v1.0 add
+		System.out.println("added by v1.0");
 	}
 }
